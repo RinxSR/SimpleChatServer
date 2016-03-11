c702cbd75a541d9a6659062cf29a90fc97b8beeb
RinxSR <rinxsr@gmail.com>
2016-03-11 14:54:19 +0300

    рассылка сообщений всем пользователям

--- src/ChatServer/serverSide/SocketHandler.java
+++ src/ChatServer/serverSide/SocketHandler.java
@@ -5,11 +5,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.*;
 
 public class SocketHandler implements Runnable {
 
-    Socket socket;
+    private Socket socket;
+    private static int connetctionCounter;
+    private static List<SocketHandler> handlers = Collections.synchronizedList(new ArrayList<>());
+    private PrintWriter out;
+    private int numberOfUser;
 
     public SocketHandler(Socket socket) {
         this.socket = socket;
@@ -17,20 +21,39 @@ public class SocketHandler implements Runnable {
 
     @Override
     public void run() {
+
         try {
+            handlers.add(this);
+            connetctionCounter++;
+            numberOfUser = connetctionCounter;
+            System.out.println("user #" + connetctionCounter + " connected");
             InputStream inputStream = socket.getInputStream();
             OutputStream outputStream = socket.getOutputStream();
 
             Scanner in = new Scanner(inputStream);
-            PrintWriter out = new PrintWriter(outputStream, true);
+            out = new PrintWriter(outputStream, true);
 
             while (true) {
-                out.println("Echo: " + in.nextLine());
+                String message = in.nextLine();
+                broadcast("user #" + numberOfUser + " said: " + message);
+                if (message.equals("exit")) {
+                    System.out.println("user #" + connetctionCounter + " disconnected");
+                    connetctionCounter--;
+                    break;
+
+                }
             }
+            socket.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public void broadcast(String message) {
+        Iterator<SocketHandler> iterator = handlers.iterator();
+        while (iterator.hasNext()) {
+            iterator.next().out.println(message);
+        }
     }
 }
